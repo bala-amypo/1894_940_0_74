9d8fd85d8174616156e273e042e086e697131ac8
bala-amypo <bala@amypo.in>
2025-12-17 06:38:54 +0000

    User commit

--- src/main/java/com/example/demo/Entity/Student.java
+++ src/main/java/com/example/demo/Entity/Student.java
@@ -7,11 +7,11 @@ public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     
-    private long id;
+    private Long id;
     private String name;
     private String email;
     
-    public Student(long id, String name, String email) {
+    public Student(Long id, String name, String email) {
         this.id = id;
         this.name = name;
         this.email = email;
@@ -20,7 +20,7 @@ public class Student {
     public Long getId() {
         return id;
     }
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
     
